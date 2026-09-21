3144b19d204ad38f3f8065924ca6f8b8bb1d7740
PepeuFBV <pedrofigueiraboaviagem@gmail.com>
2026-09-20 22:35:50 -0300

    refactor(ui): normalize Tailwind pixel utilities

--- src/components/ui/navigation-menu.tsx
+++ src/components/ui/navigation-menu.tsx
@@ -35,7 +35,8 @@ const NavigationMenuTrigger = React.forwardRef<
     React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Trigger>
 >(({ className, children, ...props }, ref) => (
     <NavigationMenuPrimitive.Trigger ref={ref} className={cn(navigationMenuTriggerStyle(), 'group', className)} {...props}>
-        {children} <ChevronDown className="relative top-[1px] ml-1 h-3 w-3 transition duration-300 group-data-[state=open]:rotate-180" aria-hidden="true" />
+        {children}{' '}
+        <ChevronDown className="relative top-[0.0625rem] ml-1 h-3 w-3 transition duration-300 group-data-[state=open]:rotate-180" aria-hidden="true" />
     </NavigationMenuPrimitive.Trigger>
 ))
 NavigationMenuTrigger.displayName = NavigationMenuPrimitive.Trigger.displayName
